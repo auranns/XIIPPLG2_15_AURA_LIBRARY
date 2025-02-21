631eb7812660fd34d71b85d34bab5b90355659e4
auranns <auranisavalent@gmail.com>
2025-02-21 22:16:09 +0700

    modul auth

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 // File: app.js
-const express = require('express');
-const categoriesRouter = require('./routes/categories');
+const express = require("express");
+const categoriesRouter = require("./routes/categories");
+const UserRouter = require("./routes/user");
+const authMiddleware = require("./middleware/authMiddleware");
+const User = require("./models/User");
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/categories', categoriesRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api", UserRouter);
+
+app.get("/profile", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.userId, {
+      attributes: { exclude: ["password"] },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user data" });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
